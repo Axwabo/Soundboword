f8c2f25d58926ccf7ca7ce4859317c7ff4905891
Axwabo <axwabo@gmail.com>
2026-06-03 18:27:41 +0200

    DBus attempt

--- Soundboword/ViewModels/MainWindowViewModel.cs
+++ Soundboword/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Media;
 using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Soundboword.Services;
+using Tmds.DBus.Protocol;
+using Tmds.DBus.SourceGenerator;
 
 namespace Soundboword.ViewModels;
 
@@ -28,11 +34,18 @@ public partial class MainWindowViewModel : ViewModelBase
 
     public ObservableCollection<SoundViewModel> Sounds { get; } = [];
 
+    [ObservableProperty]
+    public partial IBrush PressedBrush { get; set; } = Brushes.Gray;
+
     public MainWindowViewModel()
     {
     }
 
-    public MainWindowViewModel(HostControl host) => _host = host;
+    public MainWindowViewModel(HostControl host)
+    {
+        _host = host;
+        _ = TestDBus().ConfigureAwait(false);
+    }
 
     [RelayCommand]
     private async Task AddSound()
@@ -47,6 +60,20 @@ public partial class MainWindowViewModel : ViewModelBase
             Path = files[0].TryGetLocalPath()!,
             Name = Path.GetFileNameWithoutExtension(files[0].TryGetLocalPath()!)
         });
+    }
+
+    // TODO: upgrade Tmds.DBus when a new Avalonia version is released (new version included in Avalonia master 2 days ago)
+    private async Task TestDBus()
+    {
+        using var connection = new DBusConnection(DBusAddress.Session!);
+        await connection.ConnectAsync();
+        var sender = (connection.UniqueName ?? "").TrimStart(':').Replace(".", "_");
+        var token = "Soundboword_" + Stopwatch.GetTimestamp();
+        ObjectPath path = $"/org/freedesktop/portal/desktop/request/{sender}/{token}";
+        var proxy = new OrgFreedesktopImplPortalGlobalShortcutsProxy(connection.AsConnection(), "org.freedesktop.portal.GlobalShortcuts", "/org/greedesktop/portal/desktop");
+        var (sessionResponse, whatever) = await proxy.CreateSessionAsync(path, $"/org/freedesktop/portal/desktop/session/{sender}/{token}", "Soundboword", []);
+        Console.WriteLine();
+        // var session = await shortcuts.CreateSessionAsync(path, path, "Soundboword", []);
     }
 
 }
