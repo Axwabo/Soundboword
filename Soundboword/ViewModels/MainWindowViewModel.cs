567d0557d1eb1c867150d970341cd65e0fc3980e
Axwabo <axwabo@gmail.com>
2026-06-03 18:42:04 +0200

    dbus attempt numero dos

--- Soundboword/ViewModels/MainWindowViewModel.cs
+++ Soundboword/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
@@ -65,13 +66,22 @@ public partial class MainWindowViewModel : ViewModelBase
     // TODO: upgrade Tmds.DBus when a new Avalonia version is released (new version included in Avalonia master 2 days ago)
     private async Task TestDBus()
     {
+        // if (TopLevel.GetTopLevel(_host?.Host)?.TryGetPlatformHandle() is not {Handle: var windowHandle, HandleDescriptor: var handleDescriptor})
+        // return;
         using var connection = new DBusConnection(DBusAddress.Session!);
         await connection.ConnectAsync();
         var sender = (connection.UniqueName ?? "").TrimStart(':').Replace(".", "_");
         var token = "Soundboword_" + Stopwatch.GetTimestamp();
-        ObjectPath path = $"/org/freedesktop/portal/desktop/request/{sender}/{token}";
-        var proxy = new OrgFreedesktopPortalGlobalShortcutsProxy(connection.AsConnection(), "org.freedesktop.portal.GlobalShortcuts", "/org/freedesktop/portal/desktop");
-        var sesionPath = await proxy.CreateSessionAsync([]);
+        string path = $"/org/freedesktop/portal/desktop/request/{sender}/{token}";
+        var proxy = new OrgFreedesktopPortalGlobalShortcutsProxy(connection.AsConnection(), "org.freedesktop.portal.GlobalShortcuts", path);
+        var session = await proxy.CreateSessionAsync([]);
+        var requestHandle = await proxy.BindShortcutsAsync(session, [
+            ("Activate", new Dictionary<string, VariantValue>
+            {
+                {"description", "Amogus activation"}
+            })
+        ], "", []);
+        await proxy.WatchActivatedAsync(((exception, tuple) => Console.WriteLine("activated")));
         Console.WriteLine();
         // var session = await shortcuts.CreateSessionAsync(path, path, "Soundboword", []);
     }
