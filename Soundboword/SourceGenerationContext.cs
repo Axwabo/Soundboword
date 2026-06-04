fd2a9b2f0b838197ea865d6e563d36307a488651
Axwabo <axwabo@gmail.com>
2026-06-04 14:23:44 +0200

    Revert "aot attempt 2"
    This reverts commit 059a0cf85053fc6e871d77d41cbbfba3fc247f97.

--- Soundboword/SourceGenerationContext.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
-using Soundboword.Models;
-
-namespace Soundboword;
-
-[JsonSourceGenerationOptions]
-[JsonSerializable(typeof(IEnumerable<SoundDto>))]
-internal partial class SourceGenerationContext : JsonSerializerContext;
--- Soundboword/UserData.cs
+++ Soundboword/UserData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using Soundboword.Models;
 using Soundboword.ViewModels;
@@ -12,13 +13,15 @@ namespace Soundboword;
 public static class UserData
 {
 
+    private static readonly JsonSerializerOptions Options = new() {Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping};
+
     private static readonly string Folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Soundboword");
 
     private static readonly string Sounds = Path.Combine(Folder, "sounds.json");
 
     private static void EnsureDirectory() => Directory.CreateDirectory(Folder);
 
-    public static IEnumerable<SoundDto> LoadSounds()
+    public static IReadOnlyList<SoundDto> LoadSounds()
     {
         EnsureDirectory();
         if (!File.Exists(Sounds))
@@ -26,7 +29,7 @@ public static class UserData
         try
         {
             using var file = File.OpenRead(Sounds);
-            return JsonSerializer.Deserialize<IEnumerable<SoundDto>>(file, SourceGenerationContext.Default.IEnumerableSoundDto) ?? [];
+            return JsonSerializer.Deserialize<IReadOnlyList<SoundDto>>(file, Options) ?? [];
         }
         catch (Exception)
         {
@@ -38,7 +41,7 @@ public static class UserData
     {
         EnsureDirectory();
         using var file = File.Create(Sounds);
-        JsonSerializer.Serialize(file, sounds.Select(e => new SoundDto(e.Name, e.Path, e.Mode, e.Loop)), SourceGenerationContext.Default.IEnumerableSoundDto);
+        JsonSerializer.Serialize(file, sounds.Select(e => new SoundDto(e.Name, e.Path, e.Mode, e.Loop)), Options);
     }
 
 }
