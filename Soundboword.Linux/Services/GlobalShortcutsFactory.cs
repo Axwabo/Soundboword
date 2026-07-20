4a226e93fc11cec37cd8f955fa2e7dc01416b8d6
Axwabo <axwabo@gmail.com>
2026-07-20 11:57:11 +0200

    attempt dbus connection

--- Soundboword.Linux/Services/GlobalShortcutsFactory.cs
+++ Soundboword.Linux/Services/GlobalShortcutsFactory.cs
@@ -1,4 +1,6 @@
+using Soundboword.Generated;
 using Soundboword.Inputs;
+using Tmds.DBus.Protocol;
 
 namespace Soundboword.Linux.Services;
 
@@ -6,9 +8,34 @@ namespace Soundboword.Linux.Services;
 public sealed class GlobalShortcutsFactory : IInputFactory
 {
 
+    private readonly DBusConnection? _connection;
+    private readonly GlobalShortcuts? _shortcuts;
+
+    public GlobalShortcutsFactory()
+    {
+        var oldContext = SynchronizationContext.Current;
+        try
+        {
+            SynchronizationContext.SetSynchronizationContext(null);
+            _connection = new DBusConnection(new DBusConnectionOptions(DBusAddress.Session!) {AutoConnect = false});
+            _connection.ConnectAsync().AsTask().Wait();
+            _shortcuts = new GlobalShortcuts(_connection, "org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop");
+            var sessionHandleToken = $"soundboword_{Guid.CreateVersion7()}\0";
+            var session = _shortcuts.CreateSessionAsync(new Dictionary<string, VariantValue>
+            {
+                {"handle_token", $"/org/freedesktop/portal/desktop/session/Soundboword/{sessionHandleToken}"},
+                {"session_handle_token", sessionHandleToken}
+            }).Result;
+        }
+        finally
+        {
+            SynchronizationContext.SetSynchronizationContext(oldContext);
+        }
+    }
+
     public string Name => "XDG Global Shortcuts";
 
-    public bool IsAvailable => true;
+    public bool IsAvailable => _connection != null;
 
     public IInputMethod? Activate() => null;
 
---
+++ Soundboword.Linux/Services/GlobalShortcutsMethod.cs
@@ -0,0 +1,12 @@
+using Soundboword.Inputs;
+
+namespace Soundboword.Linux.Services;
+
+public sealed class GlobalShortcutsMethod : IInputMethod
+{
+
+    public void Dispose()
+    {
+    }
+
+}
