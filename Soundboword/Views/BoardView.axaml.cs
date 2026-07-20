25790bbd5be08e0e82e2f211fc41c0ececbea687
Axwabo <axwabo@gmail.com>
2026-07-20 11:31:58 +0200

    mostly fix drag-and-drop

--- Soundboword/Views/BoardView.axaml.cs
+++ Soundboword/Views/BoardView.axaml.cs
@@ -1,33 +1,55 @@
 using Avalonia.Input;
+using Avalonia.Platform.Storage;
+
+// ReSharper disable UnusedParameter.Local
+
+// ReSharper disable UnusedMember.Local
 
 namespace Soundboword.Views;
 
-public partial class BoardView : UserControl
+public sealed partial class BoardView : UserControl
 {
+
+    private static bool AllowDrop(DragEventArgs e) => e.Source is Panel presenter && DragDrop.GetAllowDrop(presenter);
+
+    private static IEnumerable<string> MapPaths(IStorageItem[] items)
+    {
+        foreach (var item in items)
+        {
+            var path = item.TryGetLocalPath();
+            if (!string.IsNullOrEmpty(path) && (path.EndsWith(".mp3") || path.EndsWith(".wav")))
+                yield return path;
+        }
+    }
 
     public BoardView() => InitializeComponent();
 
+    private BoardViewModel? Model => DataContext as BoardViewModel;
+
     private void InputElement_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
-    {
-        if (DataContext is BoardViewModel board)
-            board.Editor.Context.Close();
-    }
+        => Model?.Editor.Context.Close();
 
     private void OnDragOver(object? sender, DragEventArgs e)
     {
-        e.DragEffects = e.DataTransfer.Formats.Contains(DataFormat.File) ? DragDropEffects.Copy : DragDropEffects.None;
+        var contains = e.DataTransfer.Formats.Contains(DataFormat.File);
+        e.DragEffects = contains ? DragDropEffects.Copy : DragDropEffects.None;
+        Model?.DragOver = contains;
+    }
+
+    private void OnDragLeave(object? sender, DragEventArgs e)
+    {
+        if (AllowDrop(e))
+            Model?.DragOver = false;
     }
 
     private void OnDrop(object? sender, DragEventArgs e)
     {
-        if (!e.DataTransfer.Formats.Contains(DataFormat.File)) return;
+        Model?.DragOver = false;
+        if (!e.DataTransfer.Formats.Contains(DataFormat.File))
+            return;
         var files = e.DataTransfer.TryGetFiles();
         if (files != null)
-        {
-            foreach (var file in files)
-            {
-            }
-        }
+            Model?.List.Add(MapPaths(files));
     }
 
 }
