dd3142f3de83ca4727790e3039814e99969c4334
Axwabo <axwabo@gmail.com>
2026-07-25 14:37:38 +0200

    actually fix it?
    just had to restart pipewire

--- Soundboword.Linux/PipeWire/LinkRepair.cs
+++ Soundboword.Linux/PipeWire/LinkRepair.cs
@@ -10,25 +10,29 @@ public sealed class LinkRepair : DeviceSwitchHandler
 
     public LinkRepair(NodeManager nodeManager) => _nodeManager = nodeManager;
 
-    protected override async Task OnOutputDeviceSwitchedAsync()
+    public override async Task OnOutputDeviceSwitchedAsync()
     {
+        IsSwitching = true;
         await Task.Delay(200);
         var output = _nodeManager.OutputNode;
         var hearMyself = _nodeManager.HearMyself;
         await _nodeManager.RefreshAsync(_nodeManager.HearSounds?.IsLinked, _nodeManager.MicSounds?.IsLinked, _nodeManager.MicPassthrough?.IsLinked, _nodeManager.HearMyself?.IsLinked);
         if (output != _nodeManager.OutputNode && hearMyself is {IsLinked: true})
             await hearMyself.ToggleLink(false, _nodeManager.Links);
+        IsSwitching = false;
     }
 
-    protected override async Task OnMicrophoneSwitchedAsync()
+    public override async Task OnMicrophoneSwitchedAsync()
     {
-        if (_previousMic == _nodeManager.PhysicalMicrophone)
+        if (IsSwitching || _previousMic == _nodeManager.PhysicalMicrophone)
             return;
+        IsSwitching = true;
         _previousMic = _nodeManager.PhysicalMicrophone;
         var disconnectPassthrough = _nodeManager.MicPassthrough?.ToggleLink(false, _nodeManager.Links) ?? Task.CompletedTask;
         var disconnectHearMyself = _nodeManager.HearMyself?.ToggleLink(false, _nodeManager.Links) ?? Task.CompletedTask;
         var (passthrough, hearMyself) = _nodeManager.UpdatePhysicalMic(_nodeManager.MicPassthrough?.IsLinked, _nodeManager.HearMyself?.IsLinked);
         await Task.WhenAll(disconnectPassthrough, disconnectHearMyself, passthrough, hearMyself);
+        IsSwitching = false;
     }
 
 }
