d010321ef7c0f9108dbd994019b8b57d19f34e0a
Axwabo <axwabo@gmail.com>
2026-07-27 18:25:09 +0200

    tone down warning color

--- Soundboword/Converters/LogLevelConverter.cs
+++ Soundboword/Converters/LogLevelConverter.cs
@@ -9,15 +9,15 @@ public sealed class LogLevelConverter : IValueConverter
 {
 
     private static readonly IImmutableSolidColorBrush Info = new ImmutableSolidColorBrush(Color.FromArgb(255, 0, 0, 100));
+    private static readonly IImmutableSolidColorBrush Warning = new ImmutableSolidColorBrush(Color.FromArgb(255, 120, 100, 0));
     private static readonly IImmutableSolidColorBrush Error = new ImmutableSolidColorBrush(Color.FromArgb(255, 150, 0, 0));
-    private static readonly IImmutableSolidColorBrush Warning = new ImmutableSolidColorBrush(Color.FromArgb(255, 150, 120, 0));
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => (value as LogLevel?) switch
     {
         LogLevel.Debug => Brushes.Black,
         LogLevel.Information => Info,
-        LogLevel.Error => Error,
         LogLevel.Warning => Warning,
+        LogLevel.Error => Error,
         _ => null
     };
 
