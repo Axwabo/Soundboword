ba6dbee6f6d290a79a15a5d316409d08b9ad876b
Axwabo <axwabo@gmail.com>
2026-07-27 18:32:24 +0200

    improve settings label padding

--- Soundboword/Settings/SettingsManagerView.axaml.cs
+++ Soundboword/Settings/SettingsManagerView.axaml.cs
@@ -1,14 +1,8 @@
-using Avalonia.Interactivity;
-
 namespace Soundboword.Settings;
 
 public sealed partial class SettingsManagerView : UserControl
 {
 
     public SettingsManagerView() => InitializeComponent();
-
-    private void Control_OnLoaded(object? sender, RoutedEventArgs e)
-    {
-    }
 
 }
