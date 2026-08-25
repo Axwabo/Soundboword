329ca9e7ec009ff1adbe1f3d0dfff43386168fa7
Axwabo <axwabo@gmail.com>
2026-08-25 12:31:02 +0200

    use name enum converter in Gesture

--- Soundboword.Windows/GlobalHotkeys/Gesture.cs
+++ Soundboword.Windows/GlobalHotkeys/Gesture.cs
@@ -1,5 +1,11 @@
+using System.Text.Json.Serialization;
 using Avalonia.Input;
 
 namespace Soundboword.Windows.GlobalHotkeys;
 
-public sealed record Gesture(Key Key, KeyModifiers Modifiers);
+public sealed record Gesture(
+    [property: JsonConverter(typeof(JsonStringEnumConverter<Key>))]
+    Key Key,
+    [property: JsonConverter(typeof(JsonStringEnumConverter<KeyModifiers>))]
+    KeyModifiers Modifiers
+);
