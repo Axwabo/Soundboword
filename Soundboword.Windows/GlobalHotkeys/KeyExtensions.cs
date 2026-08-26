f5a5fb9d79dfa2005220cd00a0443082a5cfb28e
Axwabo <axwabo@gmail.com>
2026-08-26 14:40:43 +0200

    dictionary

--- Soundboword.Windows/GlobalHotkeys/KeyExtensions.cs
+++ Soundboword.Windows/GlobalHotkeys/KeyExtensions.cs
@@ -5,22 +5,40 @@ namespace Soundboword.Windows.GlobalHotkeys;
 public static class KeyExtensions
 {
 
+    private static readonly Dictionary<Key, string> Translations = new()
+    {
+        {Key.Up, "Up Arrow"},
+        {Key.Down, "Down Arrow"},
+        {Key.Left, "Left Arrow"},
+        {Key.Right, "Right Arrow"},
+        {Key.Space, "Space"},
+        {Key.Enter, "Enter"},
+        {Key.Back, "Backspace"},
+        {Key.Insert, "Insert"},
+        {Key.Delete, "Delete"},
+        {Key.Home, "Home"},
+        {Key.End, "End"},
+        {Key.PageUp, "PageUp"},
+        {Key.PageDown, "PageDown"},
+        {Key.Escape, "Escape"},
+        {Key.CapsLock, "Caps Lock"},
+        {Key.NumLock, "Num Lock"},
+        {Key.NumPad0, "Numpad 0"},
+        {Key.NumPad1, "Numpad 1"},
+        {Key.NumPad2, "Numpad 2"},
+        {Key.NumPad3, "Numpad 3"},
+        {Key.NumPad4, "Numpad 4"},
+        {Key.NumPad5, "Numpad 5"},
+        {Key.NumPad6, "Numpad 6"},
+        {Key.NumPad7, "Numpad 7"},
+        {Key.NumPad9, "Numpad 9"}
+        // TODO: might be missing some translations
+    };
+
     extension(Key key)
     {
 
-        public bool HasTranslationOverride
-            => key is Key.Space
-                or Key.Enter
-                or Key.NumPad0
-                or Key.NumPad1
-                or Key.NumPad2
-                or Key.NumPad3
-                or Key.NumPad4
-                or Key.NumPad5
-                or Key.NumPad6
-                or Key.NumPad7
-                or Key.NumPad8
-                or Key.NumPad9;
+        public bool HasTranslationOverride => Translations.ContainsKey(key);
 
         public KeyModifiers GetModifier() => key switch
         {
@@ -35,36 +53,10 @@ public static class KeyExtensions
     extension(KeyEventArgs eventArgs)
     {
 
-        public string Translate() => eventArgs.Key switch
-        {
-            Key.Up => "Up Arrow",
-            Key.Down => "Down Arrow",
-            Key.Left => "Left Arrow",
-            Key.Right => "Right Arrow",
-            Key.Space => "Space",
-            Key.Enter => "Enter",
-            Key.Back => "Backspace",
-            Key.Insert => "Insert",
-            Key.Delete => "Delete",
-            Key.Home => "Home",
-            Key.End => "End",
-            Key.PageUp => "PageUp",
-            Key.PageDown => "PageDown",
-            Key.Escape => "Escape",
-            Key.CapsLock => "Caps Lock",
-            Key.NumLock => "Num Lock",
-            Key.NumPad0 => "Numpad 0",
-            Key.NumPad1 => "Numpad 1",
-            Key.NumPad2 => "Numpad 2",
-            Key.NumPad3 => "Numpad 3",
-            Key.NumPad4 => "Numpad 4",
-            Key.NumPad5 => "Numpad 5",
-            Key.NumPad6 => "Numpad 6",
-            Key.NumPad7 => "Numpad 7",
-            Key.NumPad9 => "Numpad 9",
-            // TODO: might be missing some keys
-            var key => eventArgs.KeySymbol?.ToUpper() ?? key.ToString()
-        };
+        public string Translate()
+            => Translations.TryGetValue(eventArgs.Key, out var translation)
+                ? translation
+                : eventArgs.KeySymbol?.ToUpper() ?? eventArgs.Key.ToString();
 
     }
 
