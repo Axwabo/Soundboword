7367ec0e5a781630e00d9fe2a634d17cfc5e7f13
Axwabo <axwabo@gmail.com>
2026-09-05 17:28:04 +0200

    fix extension check + rename executable

--- bundle.cs
+++ bundle.cs
@@ -3,6 +3,7 @@ using System.IO.Compression;
 const string licenses = "THIRD_PARTY_LICENSES";
 const string license = "LICENSE";
 const string readme = "README.md";
+const string project = "Soundboword";
 
 using var zipFile = File.Create(args[0]);
 using var zipArchive = new ZipArchive(zipFile, ZipArchiveMode.Create);
@@ -11,8 +12,14 @@ zipArchive.CreateEntryFromFile($"../{license}", license);
 zipArchive.CreateEntryFromFile($"../{readme}", readme);
 
 foreach (var file in Directory.EnumerateFiles("."))
-    if (Path.GetExtension(file) is not ("dbg" or "pdb"))
-        zipArchive.CreateEntryFromFile(file, $"bin/{Path.GetFileName(file)}");
+{
+    if (Path.GetExtension(file) is ".dbg" or ".pdb")
+        continue;
+    var name = Path.GetFileName(file);
+    if (name.StartsWith($"{project}.{args[1]}"))
+        name = OperatingSystem.IsWindows() ? $"{project}.exe" : project;
+    zipArchive.CreateEntryFromFile(file, $"bin/{name}");
+}
 
 foreach (var file in Directory.EnumerateFiles($"../{licenses}"))
     zipArchive.CreateEntryFromFile(file, $"{licenses}/{Path.GetFileName(file)}");
